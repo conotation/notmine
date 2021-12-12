d7c12f42e2768e8f06fb37496aa5ed071dd0df97
Cnt <kkarrkid7@gmail.com>
2021-12-12 17:42:06 +0900

    [Fix] Modal layer fix

--- schedule/static/JS/login.js
+++ schedule/static/JS/login.js
@@ -1,9 +1,32 @@
-var sm;
+let sm;
+let unamewrap, lb, uname;
+let warnon;
 
 function init() {
+    warnon=false;
+    
     sm = document.getElementById("sm");
-    sm.onmouseover = over;
-    sm.onmouseout = out;
+    unamewrap = document.getElementById("unamewrap");
+    uname = document.getElementById("uname");
+    lb=document.getElementById("lb");
+
+    lb.style.visibility="hidden";
+
+    sm.addEventListener('mouseover',()=>{
+        over();
+    })
+    sm.addEventListener('mouseout',()=>{
+        out();
+    })
+
+    uname.addEventListener('focusin',()=>{
+        userNameDivStyleChange();
+        userNameStyleChange();
+    })
+    uname.addEventListener('focusout',()=>{
+        userNameDivBack();
+        userNameStyleBack();
+    })
 }
 
 function over() {
@@ -12,4 +35,37 @@ function over() {
 
 function out() {
     sm.style.backgroundColor = "#5F26FF";
+}
+
+function userNameDivStyleChange(){
+    if(!warnon){
+        unamewrap.style.backgroundColor="#e7dfff";
+        unamewrap.style.boxShadow="0px 2px 0px #5f26ff";
+    }
+}
+
+function userNameStyleChange(){
+    if(!warnon){
+        uname.style.width="90%";
+        uname.style.outline="none";
+        uname.style.backgroundColor="#e7dfff";
+        uname.style.color="#5f26ff";
+        uname.style.paddingLeft="12px";
+    }
+}
+
+function userNameDivBack(){
+    if(!warnon){
+    unamewrap.style.backgroundColor="#FFFFFF";
+    unamewrap.style.boxShadow="0px 2px 0px #ADADAD";
+    }
+}
+
+function userNameStyleBack(){
+    if(!warnon){
+    uname.style.width="100%";
+    uname.style.backgroundColor="#FFFFFF";
+    uname.style.color="black";
+    uname.style.paddingLeft="20px";
+    }
 }
